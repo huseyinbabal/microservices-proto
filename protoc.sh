8f060df37f977730b1254f8039a6db53a783bdcc
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-01-21 03:06:30 +0300

    gh action update

--- protoc.sh
+++ protoc.sh
@@ -9,7 +9,7 @@ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
-cd golang/${{ matrix.service }}
+cd golang/${SERVICE_NAME}
 go mod init \
   github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} || true
 go mod tidy
