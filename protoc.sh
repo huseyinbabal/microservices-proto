e6111ea21e3ac4adb3e01eb13824abb378269d0b
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-03-06 00:08:21 +0300

    gh action update

--- protoc.sh
+++ protoc.sh
@@ -2,6 +2,8 @@
 
 SERVICE_NAME=$1
 RELEASE_VERSION=$2
+USER_NAME=$3
+EMAIL=$4
 
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
@@ -14,9 +16,10 @@ go mod init \
   github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} || true
 go mod tidy
 cd ../../
-git config --global user.email "huseyinbabal88@gmail.com"
-git config --global user.name "Huseyin BABAL"
+git config user.name "$USER_NAME"
+git config user.email "$EMAIL"
 git add . && git commit -am "proto update" || true
+git push -u origin HEAD
 git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
   -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}" 
 git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
