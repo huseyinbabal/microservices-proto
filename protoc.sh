59d1a09bf85cac78e9416f6f3d6a62e15280a2d6
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-03-06 00:23:28 +0300

    push to main

--- protoc.sh
+++ protoc.sh
@@ -5,6 +5,10 @@ RELEASE_VERSION=$2
 USER_NAME=$3
 EMAIL=$4
 
+git config user.name "$USER_NAME"
+git config user.email "$EMAIL"
+git fetch --all && git checkout main
+
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
@@ -16,9 +20,6 @@ go mod init \
   github.com/huseyinbabal/microservices-proto/golang/${SERVICE_NAME} || true
 go mod tidy
 cd ../../
-git config user.name "$USER_NAME"
-git config user.email "$EMAIL"
-git fetch --all && git checkout main
 git add . && git commit -am "proto update" || true
 git push origin HEAD:main
 git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} \
